46ab138f7d6250ab1973dcea6d4a1eb9cad5aabf
Tony Scavone <ascavone@mediamath.com>
2018-10-29 15:14:05 -0400

    2.2

---
+++ book-problems/2-2.js
@@ -0,0 +1,75 @@
+// 2-1 Write code to remove duplicates from an unsorted linked list.
+
+class Node {
+  constructor(val){
+    this._val = val;
+    this._next = null;
+  }
+}
+
+class LinkedList {
+  constructor(){
+    this._head = null;  
+  }
+
+  append(val){
+    let currNode = new Node(val);
+
+    currNode._next = this._head;
+    this._head = currNode;
+  }
+
+  length(){
+    let currNode = this._head;
+    let len = 0;
+    while(currNode !== null){
+      len++
+      currNode = currNode._next;
+    }
+    return len;
+  }
+
+  removeDuplicates(){
+    let currNode = this._head;
+
+    while(currNode !== null){
+      let curVal = currNode._val;
+
+      let searchNode = currNode._next;
+      let previous = currNode;
+      while(searchNode !== null){
+        if(curVal === searchNode._val){
+          //delete the node
+          previous._next = searchNode._next;
+        }
+        else{
+          previous = searchNode;
+        }
+
+        searchNode = searchNode._next
+      }
+
+      currNode = currNode._next;
+    }
+  }
+
+  nextToLast(){
+    if(this._head === null){
+      return null;
+    }
+
+    if(this._head._next === null){
+      return null;
+    }
+
+    let prevNode = this._head;
+    let currNode = this._head._next;
+
+    while(currNode._next !== null){
+      prevNode = currNode;
+      currNode = currNode._next;
+    }
+    return prevNode;
+  }
+}
+
