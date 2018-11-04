88cbe242c86a2d27250f93540b143ce2f513624b
Tony Scavone <ascavone@mediamath.com>
2018-11-04 17:34:06 -0500

    just started 1.7

---
+++ book-problems/1-7.js
@@ -0,0 +1,12 @@
+"use strict";
+//1.7  Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column is set to 0.
+
+function assert(){
+  if(!x){
+    throw x.toString() + " is not true!";
+  }
+  else if(msg){
+    console.log("true: "  + msg);
+  }
+}
+
