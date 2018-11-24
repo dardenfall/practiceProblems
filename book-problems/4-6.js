bb01e4243e70596f82b2090abaa7b49a36b27b81
Tony Scavone <ascavone@mediamath.com>
2018-11-24 15:23:34 -0500

    added 4.6

--- book-problems/4-6.js
+++ book-problems/4-6.js
@@ -9,12 +9,50 @@ class Node {
     this._val = val;
     this._left = null;
     this._right = null;
-  }  
+  }   
 }
 
 class Tree{
   constructor(){
     this._root = null;
+  }
+
+  dfsNode(n, traversalNode){
+    if(traversalNode === null){
+      return false;
+    }
+
+    if(traversalNode === n){
+      return true;
+    }
+
+    return (this.dfsNode(n, traversalNode._left) || 
+            this.dfsNode(n, traversalNode._right));
+  }
+
+  findCommonAncestor(node1, node2){
+    let q = [];
+    let result = null;
+
+    q.push(this._root);
+  
+    while(q.length !== 0){
+      let tmp = q.shift();
+
+      if(this.dfsNode(node1, tmp) && this.dfsNode(node2, tmp)){
+        result = tmp;
+      }
+
+      if(tmp._left !== null) {
+        q.push(tmp._left);
+      }
+      if(tmp._right !== null) {
+        q.push(tmp._right);
+      }
+      
+    }
+
+    return result;
   }
 
   insert(val){
@@ -62,3 +100,18 @@ const assert = function(test, msg){
   }
   console.log(message);
 } 
+
+let t = new Tree();
+t.insert(1);
+t.insert(2);
+t.insert(3);
+t.insert(4);
+t.insert(5);
+t.insert(6);
+t.insert(7);
+assert(t.findCommonAncestor(t._root._left,t._root._right) === t._root,
+       "t.findCommonAncestor(t._root._left,t._root._right) === t._root");
+assert(t.findCommonAncestor(t._root._left._left,t._root._left._right) === t._root._left,
+       "t.findCommonAncestor(t._root._left._left,t._root._left._right) === t._root._left");       
+assert(t.findCommonAncestor(t._root._left,t._root._left._right) === t._root,
+       "t.findCommonAncestor(t.findCommonAncestor(t._root._left,t._root._left._right) === t._root");       
