3cde9334e5120ef952c44ab9e2c9fd1f45f0e8f2
Tony Scavone <ascavone@mediamath.com>
2018-11-28 21:22:32 -0500

    in progress 4-8

--- book-problems/4-8.js
+++ book-problems/4-8.js
@@ -1,4 +1,6 @@
 "use strict";
+/// IN PROGRESS
+
 // 4.8 You are given a binary tree in which each node contains a value. Design an algorithm to print 
 //   all paths which sum up to that value. 
 //   Note that it can be any path in the tree - it does not have to start at the root.
